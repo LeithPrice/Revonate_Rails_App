73312f4bf7abc3097fc098d374abed4796fc0b90
LeithPrice <leith.price@gmail.com>
2022-07-13 15:44:58 +1000

    add condition enum

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,13 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_rich_text :description
+
+  enum condition: {
+    Repairable_writeoff: 1,
+    Statutory_writeoff: 2,
+    Vintage_fixer_upper: 3,
+    Mechanical_repair_needed: 4
+  }
+
+
 end
